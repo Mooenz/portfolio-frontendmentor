5ccd61b0bd7fa5000fc673239383fded5333dd46
Mooenz <joss92821@hotmail.com>
2021-05-31 21:09:22 -0500

    Fixed background input, its transparent now

--- base-apparel-coming-soon-master/index.js
+++ base-apparel-coming-soon-master/index.js
@@ -1,5 +1,4 @@
 const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-const form = document.querySelector('.form');
 const label = document.querySelector('.label');
 const email = document.getElementById('email');
 const icon_error = document.querySelector('.icon-error');
@@ -7,13 +6,13 @@ const button = document.querySelector('.button');
 
 const checkEmail = () => {
   if(regex.test(email.value)) {  
-    form.classList = 'form true';
+    email.classList = 'email true';
     label.classList = 'label label-true';
     icon_error.classList = 'icon-error';
     label.classList = 'label label-true visibility-true';
     label.innerHTML = 'Your email its correct.';
   } else {
-    form.classList = 'form error';
+    email.classList = 'email error';
     label.classList = 'label visibility-true';
     label.innerHTML = 'Please provide a valid email.';
     icon_error.classList = 'icon-error visibility-true';
