af0232ca7dba054d39addef319aa5db22a87a4a0
Mooenz <joss92821@hotmail.com>
2021-09-08 17:03:42 -0500

    Add formatting to file

--- blogr-landing-page-main/index.js
+++ blogr-landing-page-main/index.js
@@ -1,34 +1,34 @@
 // Toogle hamburger button
 function toggleHamburger() {
-  const HAMBURGER = document.querySelector('.hamburger');
-  const HEADER__NAV = document.querySelector('.header__nav');
+  const HAMBURGER = document.querySelector(".hamburger");
+  const HEADER__NAV = document.querySelector(".header__nav");
 
-  const CLOSE_ICON = './css/images/icon-close.svg';
-  const HAMBURGER_ICON = './css/images/icon-hamburger.svg';
+  const CLOSE_ICON = "./css/images/icon-close.svg";
+  const HAMBURGER_ICON = "./css/images/icon-hamburger.svg";
 
-  const ICON = HAMBURGER.getAttribute('src');
+  const ICON = HAMBURGER.getAttribute("src");
 
   if (ICON === CLOSE_ICON) {
-    HAMBURGER.setAttribute('src', `${HAMBURGER_ICON}`);    
+    HAMBURGER.setAttribute("src", `${HAMBURGER_ICON}`);
   } else {
-    HAMBURGER.setAttribute('src', `${CLOSE_ICON}`);
+    HAMBURGER.setAttribute("src", `${CLOSE_ICON}`);
   }
-  
-  HAMBURGER.classList.toggle('hamburger--size');
-  HEADER__NAV.classList.toggle('show');
+
+  HAMBURGER.classList.toggle("hamburger--size");
+  HEADER__NAV.classList.toggle("show");
 }
 
 // Toogle Sub List
-const LIST = document.querySelectorAll('.list');
+const LIST = document.querySelectorAll(".list");
 
 LIST.forEach((list_two) => {
   const faqToggle = () => {
     LIST.forEach((item) => {
-      if(item  !== list_two) {
-        item.classList.remove('list--styles');
+      if (item !== list_two) {
+        item.classList.remove("list--styles");
       }
-    })
-    list_two.classList.toggle('list--styles');
-  }
-  list_two.addEventListener('click', faqToggle, false);
-})
+    });
+    list_two.classList.toggle("list--styles");
+  };
+  list_two.addEventListener("click", faqToggle, false);
+});
