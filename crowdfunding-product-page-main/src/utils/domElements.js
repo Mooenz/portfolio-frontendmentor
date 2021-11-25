b8a0824d0769fa070f8a200a7f006ffee72b9282
mooenz <joss92821@hotmail.com>
2021-11-24 19:55:17 -0500

    format  files

--- crowdfunding-product-page-main/src/utils/domElements.js
+++ crowdfunding-product-page-main/src/utils/domElements.js
@@ -1,7 +1,6 @@
 const $ = (selector) => document.querySelector(selector);
 const $all = (selector) => document.querySelectorAll(selector);
 
-
 const $body = $('body');
 const $product_buttons = $('.product__buttons');
 const $modal_card = $all('.modal__card');
